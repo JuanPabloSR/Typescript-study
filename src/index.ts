7508011bceca397dbeb10ad0d28d40702f415f33
Juan Pablo Sánchez Rey <juanpablosanchezrey22@gmail.com>
2023-01-28 09:22:16 -0500

    03-functions

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,20 @@ console.log("Hola Mundo!!!!");
     ===== Código de TypeScript =====
 */
 
+function add(a: number, b:number) {
+  return a + b;
+}
+
+function multiply (number: number, otherNumber?:number, base:number=2):number {  //? El argumento requerido no puede seguir depues de uno opcional
+    return number * base;
+}
+
+
+const addArrow = (a, b) => {
+  return a + b;
+}
+
+
+const result = multiply(30, 20);
+
+console.log(result);
